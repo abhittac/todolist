f528ba14ff4713ef42fbe238964f3b2ceed2b941
abhittac <59259249+abhittac@users.noreply.github.com>
2020-07-02 18:24:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 //take user input and Add button store user input
 
+
 document.getElementById("add").addEventListener("click",getdata);
+
 function getdata(e){
   
 
-var user=document.getElementById("user").value;
-  
 //create li append user data
+if(user=="")
+{
+  alert("Add Some Task!!!")
+}
+else{
+  var user=document.getElementById("user").value;
 var li=document.createElement("li");
 li.innerText=user;
+
 var btn=document.createElement("i");
 btn.className="fa fa-close";
 li.append(btn);
 //append into parent 
-var one=document.querySelector("#parent").append(li);
-e.stopPropagation();
+document.querySelector("#parent").append(li);
+document.getElementById("user").value="";
+}
 };
 
 //remove function add event on parent
